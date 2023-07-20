1e67e95f70ed25e90c9fcc33d12feda544cca2c0
Nathan Sant Helena <ice44xx@gmail.com>
2023-07-20 13:00:03 -0300

    troca de nome das img

--- src/components/common/donations/index.tsx
+++ src/components/common/donations/index.tsx
@@ -8,8 +8,8 @@ const Donations = () => {
             <p>Se você gosta e quer apoiar nosso trabalho e ajudar a mantar o site sempre atualizado e sem anúncios</p>
             <Link href={'https://link.mercadopago.com.br/nekoanimes'} className={styles.link} target="blank">
                 <div className={styles.background}>
-                    <img src="/assets/teste.png" className={styles.head} alt="logo neko animes" />
-                    <img src="/assets/teste1.png" className={styles.body} alt="logo neko animes" />
+                    <img src="/assets/head.png" className={styles.head} alt="logo neko animes" />
+                    <img src="/assets/body.png" className={styles.body} alt="logo neko animes" />
                     <div className={styles.donation}>
                         <img src="/assets/donations/coffe.gif" className={styles.coffe} alt="" />
                         <p>Apoie a Neko Animes</p>
